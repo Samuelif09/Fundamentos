6bf4f5509a6fdc7accc219da070548f5b1bc2ec9
Nicolas Silva Garcia <silvag.n@javeriana.edu.co>
2026-05-22 16:29:48 -0500

    Guardar libros en memoria para mostrar por medio del protocolo GET

--- backend/src/main/java/com/openlib/market/infrastructure/autenticacion/AuthDummyPasswordEncoderAdapter.java
+++ backend/src/main/java/com/openlib/market/infrastructure/autenticacion/AuthDummyPasswordEncoderAdapter.java
@@ -3,14 +3,15 @@ package com.openlib.market.infrastructure.autenticacion;
 import com.openlib.market.domain.autenticacion.IVerificadorPasswordGateway;
 import com.openlib.market.domain.autenticacion.PasswordPlano;
 import org.springframework.stereotype.Component;
+import java.util.Base64; // <-- Importante añadir este import
 
 @Component
 public class AuthDummyPasswordEncoderAdapter implements IVerificadorPasswordGateway {
 
     @Override
     public boolean verificar(PasswordPlano passwordPlano, String hashAlmacenado) {
-        // En la Entrega 1, la verificación es un match exacto si no hay bcrypt integrado
-        // En Entrega 2, esto usaría BCrypt.checkpw(passwordPlano.getValor(), hashAlmacenado)
-        return passwordPlano.getValor().equals(hashAlmacenado);
+        String passDecodificado = new String(Base64.getDecoder().decode(hashAlmacenado));
+
+        return passwordPlano.getValor().equals(passDecodificado);
     }
 }
